1e28ee17866573d75b7b4a866abc9eb97ade016b
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-02-27 21:48:41 +0000

    added logic to the file

--- index.js
+++ index.js
@@ -13,58 +13,150 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const questions = [
-// 	* When a user starts the application then they are prompted to enter the **team manager**’s:
-{
-	// 	* Name
-	type: "input",
-	name: "managerName",
-	message: "Enter the team manager's name:",
-	default: "John Appleseed",
-	filter: String,
-},
-{
-	// 	* Employee ID
-	type: "input",
-	name: "managerID",
-	message: "Enter the team manager's employee ID:",
-	default: "202",
-	validate(value) {
-      const valid = !isNaN(parseFloat(value));
-      return valid || "Please enter a number.";
+const managerQuestions = [
+	{
+		type: "input",
+		name: "managerName",
+		message: "Enter the team manager's name:",
+		default: "John Appleseed",
 	},
-	filter: Number,
-},
-	// 	* Email address
-	// 	* Office number
-
-
-//  * When a user enters those requirements then the user is presented with a menu with the option to:
-	// 	* Add an engineer
-	// 	* Add an intern 
-	// 	* Finish building the team
-//  * When a user selects the **engineer** option then a user is prompted to enter the following and then the user is taken back to the menu:
-	// 	* Engineer's Name
-	// 	* ID
-	// 	* Email
-	// 	* GitHub username
-//  * When a user selects the intern option then a user is prompted to enter the following and then the user is taken back to the menu:
-	// 	* Intern’s name
-	// 	* ID
-	// 	* Email
-	// 	* School
-//  * When a user decides to finish building their team then they exit the application, and the HTML is generated.
+	{
+		type: "input",
+		name: "managerID",
+		message: "Enter the team manager's employee ID:",
+		default: "202",
+	},
+	{
+		type: "input",
+		name: "managerEmail",
+		message: "Enter the team manager's email address:",
+		default: "john.appleseed@gmail.com",
+	},
+	{
+		type: "input",
+		name: "managerOfficeNumber",
+		message: "Enter the team manager's office number:",
+		default: "41",
+	},
 ];
 
-inquirer
-  .prompt(questions)
-  .then((answers) => {
-    	// Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-		if (error.isTtyError) {
-			// Prompt couldn't be rendered in the current environment
-		} else {
-			// Something else went wrong
-		}
-  });
+
+const addTeamMenuQuestions = [
+	{
+		type: "list",
+		name: "optionsMenu",
+		message: "Please select an option:",
+		choices: ["Add an engineer.", "Add an intern.", "Finish building the team."],
+	},
+];
+
+const engineerQuestions = [
+	{
+		type: "input",
+		name: "engineerName",
+		message: "Enter the engineer's name:",
+		default: "John Appleseed",
+	},
+	{
+		type: "input",
+		name: "engineerID",
+		message: "Enter the engineer's employee ID:",
+		default: "202",
+	},
+	{
+		type: "input",
+		name: "engineerEmail",
+		message: "Enter the engineer's email address:",
+		default: "john.appleseed@gmail.com",
+	},
+	{
+		type: "input",
+		name: "engineerGithub",
+		message: "Enter the engineer's GitHub link:",
+		default: "https://github.com/johnAppleseed",
+	},
+];
+
+const internQuestions = [
+	{
+		type: "input",
+		name: "internName",
+		message: "Enter the intern's name:",
+		default: "John Appleseed",
+	},
+	{
+		type: "input",
+		name: "internID",
+		message: "Enter the intern's employee ID:",
+		default: "202",
+	},
+	{
+		type: "input",
+		name: "internEmail",
+		message: "Enter the intern's email address:",
+		default: "john.appleseed@gmail.com",
+	},
+	{
+		type: "input",
+		name: "internSchool",
+		message: "Enter the intern's school:",
+		default: "UCLA",
+	},
+];
+
+const employeeQuestions = [
+	{
+		type: "input",
+		name: "employeeName",
+		message: "Enter the employee's name:",
+		default: "John Appleseed",
+	},
+	{
+		type: "input",
+		name: "employeeID",
+		message: "Enter the employee's employee ID:",
+		default: "202",
+	},
+	{
+		type: "input",
+		name: "employeeEmail",
+		message: "Enter the employee's email address:",
+		default: "john.appleseed@gmail.com",
+	},
+];
+
+
+const managerAnswers = [];
+const engineerAnswers = [];
+const internAnswers = [];
+const employeeAnswers = [];
+
+
+inquirer.prompt(managerQuestions).then((answers) => {
+	managerAnswers.push(answers);
+	inquirer.prompt(addTeamMenuQuestions).then((answers) => {
+		console.log("\n");
+		console.log(answers.optionsMenu);
+		if (answers.optionsMenu == "Add an engineer.") {
+			//  TODO: When a user selects the **engineer** option then a user is prompted to enter the following and then the user is taken back to the menu:
+			inquirer.prompt(engineerQuestions).then((answers) => {
+				engineerAnswers.push(answers);
+			})
+		} else if (answers.optionsMenu == "Add an intern.") {
+			//  TODO: When a user selects the intern option then a user is prompted to enter the following and then the user is taken back to the menu:
+			inquirer.prompt(internQuestions).then((answers) => {
+				internAnswers.push(answers)
+			})
+		} else if (answers.optionsMenu == "Finish building the team.") {
+			inquirer.prompt(employeeQuestions).then((answers) => {
+				employeeAnswers.push(answers)
+			})
+		};
+	});	
+});
+
+
+
+
+
+//  TODO: When a user decides to finish building their team then they exit the application, and the HTML is generated.
